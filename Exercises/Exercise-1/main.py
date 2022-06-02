3440ecf787eeeff5cd4fbb1aacba80e3b4649f28
h.agourame <h.agourame@siliconesignal.com>
2022-06-02 16:17:28 +0100

    Exercice 1 correction

--- Exercises/Exercise-1/main.py
+++ Exercises/Exercise-1/main.py
@@ -1,4 +1,11 @@
-import requests
+import requests,zipfile,os,io
+from io import StringIO
+import asyncio
+import aiohttp
+import nest_asyncio
+
+nest_asyncio.apply()
+
 
 download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2018_Q4.zip',
@@ -6,15 +13,35 @@ download_uris = [
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q2.zip',
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q3.zip',
     'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2019_Q4.zip',
-    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip',
-    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2220_Q1.zip'
+    'https://divvy-tripdata.s3.amazonaws.com/Divvy_Trips_2020_Q1.zip'
 ]
 
-
-def main():
-    # your code here
-    pass
+path = 'downloads'
 
 
-if __name__ == '__main__':
-    main()
+def create_directory():
+# Check whether the specified path exists or not
+    isExist = os.path.exists(path)
+
+    if not isExist:
+        # Create a new directory because it does not exist 
+        os.makedirs(path)
+        print("The new directory is created!")
+    
+async def getFiles():
+    
+    for url in download_uris:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as response:
+
+                z = zipfile.ZipFile(io.BytesIO(await response.read()))
+                if(os.path.exists(path)):
+                    z.extractall(path)
+                    
+                else:
+                    create_directory()
+                    z.extractall(path)
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(getFiles())
+
--- Exercises/Exercise-2/main.py
+++ Exercises/Exercise-2/main.py
@@ -1,12 +1,27 @@
 import requests
 import pandas
+import html
+from bs4 import BeautifulSoup
 
 
 
 def main():
     # your code here
-    pass
+    
+    url = 'https://www.ncei.noaa.gov/data/local-climatological-data/access/2021/'
+    page = requests.get(url)
+    
+    print(page.content)
+    soup = BeautifulSoup(page.content, "html.parser")
+    results = soup.find("td").text
+    print(soup.prettify())
+    print("---------------")
+    print(soup.tr)
+    
 
 
 if __name__ == '__main__':
     main()
+
+
+
